9bd7c4c0e59909bd0795f2fd02de75996266a21a
jon-lad <jon_lad@hotmail.com>
2014-11-05 11:39:42 +0000

    Create Planet.cs

---
+++ Planet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+	public int type;
+	public bool asteroidField;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
